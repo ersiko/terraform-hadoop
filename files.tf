473ac9ee83a9f8b71b3fb969f167a67eafe7e26e
Kevin Pedersen <byte_count@hotmail.com>
2015-12-18 20:47:38 -0500

    removing useless subdir

--- files.tf
+++ files.tf
@@ -1,5 +1,5 @@
 resource "template_file" "cluster_hosts" {
-  filename = "templates/ansible/hosts"
+  filename = "templates/hosts"
   vars {
     cnode_addresses = "${join("\n", aws_instance.cnode.*.private_dns)}"
     mnode_addresses = "${join("\n", aws_instance.mnode.*.private_dns)}"
